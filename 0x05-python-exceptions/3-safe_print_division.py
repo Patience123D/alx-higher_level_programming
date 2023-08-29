6df267e5d23ecb5550ab79985ae45c78aab2674f
Patience123D <ptncdotse@gmail.com>
2023-08-28 17:35:37 -0700

    corrections

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+     try:
+        division = a / b
+    except (TypeError, ZeroDivisionError):
+        division = None
+    finally:
+        print("Inside output: {}".format(division))
+    return (division)
